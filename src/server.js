d2491214ea039276363a5bd01dd3676031323419
BrunoCotaXavier <brunocota@hotmail.com.br>
2023-06-07 15:48:30 -0300

    fix/cors

--- src/server.js
+++ src/server.js
@@ -19,22 +19,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(timeout('120s'));
 
-app.all('https://andcoy.com', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://andcoy.com");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
 const corsOptions = {
-    origin: 'https://andcoy.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200,
-    credentials: true,
-    preflightContinue: true
-};
+  };
 app.use(cors(corsOptions));
+
 app.use(product_routes);
 app.use(user_routes);
 app.use(auth_routes);
