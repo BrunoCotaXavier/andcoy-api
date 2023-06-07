d6f2a0b5cd881cba209a5c2b07859bb885e54aa4
BrunoCotaXavier <brunocota@hotmail.com.br>
2023-06-07 16:15:07 -0300

    fix/cors

--- src/server.js
+++ src/server.js
@@ -19,18 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(timeout('120s'));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
-
 const corsOptions = {
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: true
 };
 app.use(cors(corsOptions));
 
